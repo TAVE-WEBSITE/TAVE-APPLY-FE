eea4c7681e76408e17d09a87e4479d07ae534f0b
PangKiYeon <pdrkeamy@gmail.com>
2025-05-14 16:56:36 +0900

    FEAT : 지원서 작성 페이지 개발 초안

--- src/app/recruit/steps/Common.tsx
+++ src/app/recruit/steps/Common.tsx
@@ -1,5 +1,44 @@
+'use client';
+
+import Disclosure from '@/components/Disclosure';
+import TextArea from '@/components/Input/TextArea';
+import FlexBox from '@/components/layout/FlexBox';
+import ButtonNavigate from '@/components/Button/ButtonNavigate';
+import { useState } from 'react';
+import useRecruitStore from '@/store/recruitStore';
+
 const Common = () => {
-  return <div>Common Component</div>;
+    const { setCurrentStep } = useRecruitStore();
+    const [motivation, setMotivation] = useState('');
+    const [keyword, setKeyword] = useState('');
+    const [plan, setPlan] = useState('');
+    return (
+        <FlexBox direction="col" className="gap-10">
+            <h1 className="font-bold text-2xl text-[#394150] text-center">공통 질문</h1>
+            <Disclosure title="지원 동기와 TAVE 활동을 통해 얻고 싶은 것을 적어주세요." isRequired>
+                <TextArea value={motivation} setValue={setMotivation} placeholder="채워야하는 말" maxLength={700} />
+            </Disclosure>
+            <Disclosure title={'3개의 키워드로 자신을 표현해주세요.'} isRequired>
+                <TextArea value={keyword} setValue={setKeyword} placeholder="채워야하는 말" maxLength={700} />
+            </Disclosure>
+            <Disclosure title="이번 학기 계획이 있으신가요?" isRequired>
+                <TextArea value={plan} setValue={setPlan} placeholder="채워야하는 말" maxLength={700} />
+            </Disclosure>
+            <Disclosure
+                title={'아래의 목록 중 홍길동님께서 소유하고 있으신 것이 있다면\n자유롭게 첨부해주세요.'}
+                isRequired
+            >
+                채워야하는 컴포넌트
+            </Disclosure>
+            <Disclosure title="가능한 오프라인 면접 시간을 모두 체크해주세요." isRequired>
+                채워야하는 컴포넌트
+            </Disclosure>
+            <div className="flex md:flex-row flex-col md:justify-between">
+                <ButtonNavigate text="다음" onClick={() => setCurrentStep(4)} />
+                <ButtonNavigate hasBackGround={false} text="이전" onClick={() => setCurrentStep(2)} />
+            </div>
+        </FlexBox>
+    );
 };
 
 export default Common;
--- src/app/recruit/steps/Complete.tsx
+++ src/app/recruit/steps/Complete.tsx
@@ -1,5 +1,33 @@
+import ButtonNavigate from '@/components/Button/ButtonNavigate';
+import FlexBox from '@/components/layout/FlexBox';
+
+const completeData = [
+    '1. TAVE 15기 활동은 매주 토요일에 오프라인으로 진행됩니다.',
+    '2. 모든 정기 세션은 필수 참여가 원칙이며, 토요일 14시 ~ 18시 사이에 진행됩니다.',
+    '3. TAVE 16기 OT 및 MT는 8/31 (토) 이며, 만남의 장은 9/7 (토) 에 진행될 예정입니다.',
+    '4. 합격 후 OT 및 MT 참석, 만남의 장 불참으로 인한 합격 취소 시 회비 환불되지 않습니다.',
+];
+
 const Complete = () => {
-  return <div>Complete Component</div>;
+    return (
+        <FlexBox direction="col" className="text-[#394150] items-center gap-5">
+            <h3 className="font-bold text-2xl">지원 완료</h3>
+            <p className="text-center text-lg font-medium mb-5">
+                작성해주신 서류가 성공적으로 제출되면
+                <br />
+                가입하신 이메일로 지원 완료 메일 전송됩니다.
+            </p>
+            {completeData.map((data, index) => (
+                <div
+                    className="font-semibold py-6 px-5 md:w-[650px] w-[320px] border rounded-[20px] border-gray-200"
+                    key={index}
+                >
+                    {data}
+                </div>
+            ))}
+            <ButtonNavigate text="확인했습니다" onClick={() => {}} />
+        </FlexBox>
+    );
 };
 
 export default Complete;
