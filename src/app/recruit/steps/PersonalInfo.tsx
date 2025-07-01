4c9b0a12331f901194cf8f2f3df7484070b0d66a
PangKiYeon <pdrkeamy@gmail.com>
2025-07-02 07:32:06 +0900

    FIX : 개인 정보 조회 2차 디자인 및 에러 수정

--- src/app/recruit/steps/PersonalInfo.tsx
+++ src/app/recruit/steps/PersonalInfo.tsx
@@ -3,60 +3,50 @@
 import InputContainer from '@/components/layout/InputContainer';
 import InputField from '@/components/Input/InputField';
 import FlexBox from '@/components/layout/FlexBox';
-import { useRecruitStore } from '@/store/recruitStore';
-import ButtonNavigate from '@/components/Button/ButtonNavigate';
-import { RecruitField } from '@/modules/recruitType';
 import Select from '@/components/select/Select';
-import { useEffect } from 'react';
+import ButtonNavigate from '@/components/Button/ButtonNavigate';
+import { PersonalResponse, RecruitField } from '@/modules/recruitType';
+import { useEffect, useState } from 'react';
 import { useRecruit } from '@/hooks/useRecruit';
 import { useHomeStore } from '@/store/homeStore';
+import { useRecruitStore } from '@/store/recruitStore';
+import { formattedToRecruitField, recruitToFormattedField } from '@/utils/formatField';
 import { useMemberStore } from '@/store/memberStore';
-import { recruitToFormattedField } from '@/utils/formatField';
-import { formattedToRecruitField } from '@/utils/formatField';
 
-const lables = ['이름', '성별', '생년월일', '연락처', '이메일 주소'];
-const options: RecruitField[] = ['디자인', '웹 프론트엔드', '앱 프론트엔드', '백엔드', '데이터분석', '딥러닝'];
- 
 const PersonalInfo = () => {
     const { generation } = useHomeStore();
-    const { getPersonal, postApplication } = useRecruit();
-    const { username, email, memberId } = useMemberStore();
+    const { memberId } = useMemberStore();
+    const [personal, setPersonal] = useState<PersonalResponse>();
+    const { applyPersonal, makeApplication } = useRecruit();
+    const { applyField, school, major, minor, setSchool, setMajor, setMinor, setApplyField, setCurrentStep } =
+        useRecruitStore();
+
+    const options: RecruitField[] = ['디자인', '웹 프론트엔드', '앱 프론트엔드', '백엔드', '데이터분석', '딥러닝'];
 
     useEffect(() => {
         if (!memberId) return;
 
         const fetchPersonal = async () => {
-            await getPersonal(memberId);
+            const data = await applyPersonal(memberId);
+            setPersonal(data);
+            setMajor(data.major);
+            setMinor(data.minor);
+            setSchool(data.school);
+            setApplyField(formattedToRecruitField(data.field));
         };
 
         fetchPersonal();
     }, [memberId]);
 
-    const {
-        sex,
-        phoneNumber,
-        birthday,
-        school,
-        major,
-        minorDouble,
-        applyField,
-        setSchool,
-        setMajor,
-        setMinorDouble,
-        setApplyField,
-        setCurrentStep,
-    } = useRecruitStore();
-
-    const handlePostResume = async () => {
-        console.log(school, major, minorDouble, applyField, generation);
+    const handleApplication = async () => {
         if (applyField !== '선택') {
-            const res = await postApplication(
+            const res = await makeApplication(
                 {
-                    school: school,
-                    major: major,
-                    minor: minorDouble,
+                    school,
+                    major,
+                    minor,
                     field: recruitToFormattedField(applyField),
-                    generation: generation,
+                    generation,
                 },
                 memberId
             );
@@ -65,38 +55,40 @@ const PersonalInfo = () => {
             }
         }
     };
-    const formatSex = (value: string) => {
-        if (value === 'MALE') return '남자';
-        if (value === 'FEMALE') return '여자';
-        return '';
-    };
+
+    const personalFields = [
+        { label: '이름', value: personal?.username },
+        { label: '성별', value: personal?.sex === 'FEMALE' ? '여성' : '남성' },
+        { label: '생년월일', value: personal?.birthday },
+        { label: '전화번호', value: personal?.phoneNumber },
+        { label: '이메일 주소', value: personal?.email },
+    ];
 
     const handleCheck = () => {
         console.log(applyField);
-        return (school ?? '').trim().length > 0 && (major ?? '').trim().length > 0 && applyField !== null;
+        if (school.length && major.length && options.includes(applyField as RecruitField)) return true;
+        else return false;
     };
 
     return (
         <>
-            <h1 className="font-bold text-2xl text-[#394150] text-center">개인 정보 입력</h1>
-            <FlexBox direction="col" className="pt-4 gap-8">
-                <InputContainer label="이름">
-                    <InputField value={username ?? ''} disabled />
-                </InputContainer>
-                <InputContainer label="성별">
-                    <InputField value={formatSex(sex) ?? ''} disabled />
-                </InputContainer>
-                <InputContainer label="생년월일">
-                    <InputField value={birthday ?? ''} disabled />
-                </InputContainer>
-                <InputContainer label="연락처">
-                    <InputField value={phoneNumber ?? ''} disabled />
-                </InputContainer>
-                <InputContainer label="이메일 주소">
-                    <InputField value={email ?? ''} disabled />
-                </InputContainer>
-                <InputContainer label="지원분야" isRequired={true} description="기수 당 1개의 분야만 지원 가능합니다.">
-                    <Select selectedValue={applyField ?? '선택'} setSelectedValue={setApplyField}>
+            <p className="font-bold md:text-2xl text-xl text-[#394150] text-center mb-4 md:mb-7">개인 정보 입력</p>
+            <FlexBox direction="col" className="md:gap-6 gap-5">
+                {personalFields.map(({ label, value }) => (
+                    <InputContainer key={label} label={label} isRequired={false}>
+                        <div
+                            className="p-3 md:p-4 bg-gray-100 border-gray-200 border
+                            rounded-[10px] text-[#A9ACB4] cursor-not-allowed"
+                        >
+                            {value}
+                        </div>
+                    </InputContainer>
+                ))}
+                <InputContainer
+                    label="지원분야"
+                    description="1개 분야만 지원 가능하며, 변경 시 기존 내용이 삭제됩니다."
+                >
+                    <Select selectedValue={applyField || '선택'} setSelectedValue={setApplyField}>
                         <Select.Trigger>
                             <Select.Options>
                                 {options.map((option) => (
@@ -106,27 +98,17 @@ const PersonalInfo = () => {
                         </Select.Trigger>
                     </Select>
                 </InputContainer>
-                <InputContainer label="학교" isRequired={true}>
-                    <InputField value={school ?? ''} setValue={setSchool} placeholder="학교를 입력해주세요" />
+                <InputContainer label="학교">
+                    <InputField value={school} setValue={setSchool} placeholder="학교를 입력해주세요" />
                 </InputContainer>
-                <InputContainer label="전공" isRequired={true}>
-                    <InputField value={major ?? ''} setValue={setMajor} placeholder="전공을 입력해주세요" />
+                <InputContainer label="전공">
+                    <InputField value={major} setValue={setMajor} placeholder="전공을 입력해주세요" />
                 </InputContainer>
-                <InputContainer label="부전공/복수전공" isRequired={true}>
-                    <InputField
-                        value={minorDouble ?? ''}
-                        setValue={setMinorDouble}
-                        placeholder="부전공 or 복수전공을 입력해주세요"
-                    />
+                <InputContainer label="부전공/복수전공" isRequired={false}>
+                    <InputField value={minor} setValue={setMinor} placeholder="부전공/복수전공을 입력해주세요" />
                 </InputContainer>
-                <FlexBox className="justify-end">
-                    <ButtonNavigate
-                        text="다음"
-                        onClick={() => {
-                            handlePostResume();
-                        }}
-                        isActive={handleCheck()}
-                    />
+                <FlexBox className="justify-end mt-3 md:mt-2">
+                    <ButtonNavigate text="다음" onClick={handleApplication} isActive={handleCheck()} />
                 </FlexBox>
             </FlexBox>
         </>
