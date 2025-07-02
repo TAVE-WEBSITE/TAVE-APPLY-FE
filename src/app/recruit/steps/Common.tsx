23f57cc518a23b82b138ac429a6bd5ca3d5a9355
PangKiYeon <pdrkeamy@gmail.com>
2025-07-02 22:04:00 +0900

    FIX  : 임시저장 버튼 디자인 수정

--- src/app/recruit/steps/Common.tsx
+++ src/app/recruit/steps/Common.tsx
@@ -223,92 +223,94 @@ const Common = () => {
     const canTempSave = Object.values(questions).some((val) => val.trim() !== '') || selectedTimes.length > 0;
 
     return (
-        <FlexBox direction="col" className="gap-5 relative">
+        <div className="w-full h-full">
             <button
                 onClick={handleTempSave}
                 disabled={!canTempSave}
-                className="hidden md:block absolute right-15 rounded-lg border border-[#E5E7EB] bg-white text-[#394150] p-3"
+                className="hidden md:block absolute right-15 top-70 rounded-lg border border-[#E5E7EB] bg-white text-[#394150] p-3"
             >
                 임시 저장
             </button>
-            <h1 className="font-bold text-2xl text-[#394150] text-center">공통 질문</h1>
+            <FlexBox direction="col" className="gap-5 relative w-full h-full">
+                <h1 className="font-bold text-2xl text-[#394150] text-center">공통 질문</h1>
 
-            {questionList.map((q) => (
-                <Disclosure
-                    key={q.id}
-                    title={q.question}
-                    isRequired={q.isRequired}
-                    description={`(${q.textLength}자 이내)`}
-                >
-                    <TextArea
-                        value={questions[q.id] ?? ''}
-                        setValue={(val) => handleAnswerChange(q.id, val)}
-                        placeholder="지원자님의 경험을 공유해주세요"
-                        maxLength={q.textLength}
-                    />
-                </Disclosure>
-            ))}
+                {questionList.map((q) => (
+                    <Disclosure
+                        key={q.id}
+                        title={q.question}
+                        isRequired={q.isRequired}
+                        description={`(${q.textLength}자 이내)`}
+                    >
+                        <TextArea
+                            value={questions[q.id] ?? ''}
+                            setValue={(val) => handleAnswerChange(q.id, val)}
+                            placeholder="지원자님의 경험을 공유해주세요"
+                            maxLength={q.textLength}
+                        />
+                    </Disclosure>
+                ))}
 
-            <Disclosure title={'아래의 목록 중 소유하고 있으신 것이 있다면 자유롭게 첨부해주세요 :)'} isRequired>
-                <Uploader
-                    options={uploadOptions}
-                    selectedOption={selectedOption}
-                    setSelectedOption={handleOptionChange}
-                    setUploadType={setUploadType}
-                    uploadValues={uploadValues}
-                    onSaveUpload={async (option) => {
-                        const val = uploadValues[option];
-                        if (!val) return;
+                <Disclosure title={'아래의 목록 중 소유하고 있으신 것이 있다면 자유롭게 첨부해주세요 :)'} isRequired>
+                    <Uploader
+                        options={uploadOptions}
+                        selectedOption={selectedOption}
+                        setSelectedOption={handleOptionChange}
+                        setUploadType={setUploadType}
+                        uploadValues={uploadValues}
+                        onSaveUpload={async (option) => {
+                            const val = uploadValues[option];
+                            if (!val) return;
 
-                        try {
-                            if (option === 'Portfolio' && val instanceof File) {
-                                await postURL(resumeId, val);
-                            } else if (typeof val === 'string') {
-                                const githubUrl = option === 'Github' ? val : '';
-                                const blogUrl = option === 'Tech Blog' ? val : '';
-                                await postSocialLinks(resumeId, githubUrl, blogUrl);
-                            } else {
+                            try {
+                                if (option === 'Portfolio' && val instanceof File) {
+                                    await postURL(resumeId, val);
+                                } else if (typeof val === 'string') {
+                                    const githubUrl = option === 'Github' ? val : '';
+                                    const blogUrl = option === 'Tech Blog' ? val : '';
+                                    await postSocialLinks(resumeId, githubUrl, blogUrl);
+                                } else {
+                                }
+                            } catch (e) {
+                                console.error(e);
                             }
-                        } catch (e) {
-                            console.error(e);
-                        }
-                    }}
-                >
-                    <Uploader.UploadField
-                        key={selectedOption}
-                        type={uploadType}
-                        value={uploadValues[selectedOption] ?? ''}
-                        onChange={handleUploadChange}
-                        setValue={(val) => setUploadValues((prev) => ({ ...prev, [selectedOption]: val }))}
-                    />
-                </Uploader>
-            </Disclosure>
+                        }}
+                    >
+                        <Uploader.UploadField
+                            key={selectedOption}
+                            type={uploadType}
+                            value={uploadValues[selectedOption] ?? ''}
+                            onChange={handleUploadChange}
+                            setValue={(val) => setUploadValues((prev) => ({ ...prev, [selectedOption]: val }))}
+                        />
+                    </Uploader>
+                </Disclosure>
 
-            <Disclosure title="가능한 오프라인 면접 시간을 모두 체크해주세요" isRequired>
-                <TimePicker>
-                    {scheduleData.map((schedule) => (
-                        <TimePicker.DateRow key={schedule.date} date={schedule.date}>
-                            {schedule.timeSlots.map((timeSlot) => {
-                                const dateTime = `${schedule.date}T${timeSlot.time}`;
-                                return (
-                                    <TimePicker.TimeSlotButton
-                                        key={timeSlot.time}
-                                        time={timeSlot.time}
-                                        isSelected={selectedTimes.includes(dateTime)}
-                                        onClick={() => toggleTimeSelection(schedule.date, timeSlot.time)}
-                                    />
-                                );
-                            })}
-                        </TimePicker.DateRow>
-                    ))}
-                </TimePicker>
-            </Disclosure>
+                <Disclosure title="가능한 오프라인 면접 시간을 모두 체크해주세요" isRequired>
+                    <TimePicker>
+                        {scheduleData.map((schedule) => (
+                            <TimePicker.DateRow key={schedule.date} date={schedule.date}>
+                                {schedule.timeSlots.map((timeSlot) => {
+                                    const dateTime = `${schedule.date}T${timeSlot.time}`;
+                                    return (
+                                        <TimePicker.TimeSlotButton
+                                            key={timeSlot.time}
+                                            time={timeSlot.time}
+                                            isSelected={selectedTimes.includes(dateTime)}
+                                            onClick={() => toggleTimeSelection(schedule.date, timeSlot.time)}
+                                        />
+                                    );
+                                })}
+                            </TimePicker.DateRow>
+                        ))}
+                    </TimePicker>
+                </Disclosure>
 
-            <FlexBox className="justify-between mt-8 mb-0 gap-2">
-                <ButtonNavigate text="이전" onClick={() => handlePostOnly(2)} />
-                <ButtonNavigate text="제출하기" onClick={handleSubmit} />
+                <FlexBox className="justify-between mt-8 mb-0 gap-2">
+                    <ButtonNavigate text="이전" onClick={() => handlePostOnly(2)} />
+                    <ButtonNavigate text="제출하기" onClick={handleSubmit} />
+                </FlexBox>
             </FlexBox>
-        </FlexBox>
+        </div>
     );
 };
 
