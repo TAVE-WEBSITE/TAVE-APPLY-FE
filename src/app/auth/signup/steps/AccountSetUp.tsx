93f68cf61df0de2ef8f8ceefb3b1bc9e192b23fb
PangKiYeon <pdrkeamy@gmail.com>
2025-07-20 13:08:51 +0900

    FIX : 회원 가입 QA 사항 수정

--- src/app/auth/signup/steps/AccountSetUp.tsx
+++ src/app/auth/signup/steps/AccountSetUp.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { isValidPassword, validatePasswordConfirm } from '@/utils/validate';
 import { useValidation } from '@/hooks/useValidation';
 import { useSignUpStore } from '@/store/signUpStore';
@@ -42,6 +42,8 @@ const AccountSetUp = () => {
 
     const [isToastOpen, setIsToastOpen] = useState(false);
     const [toast, setToast] = useState({ message: '', isError: false });
+    const [descriptionChange, setDescriptionChange] = useState(false);
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
 
     const handleCheck = () => {
         if (
@@ -56,6 +58,22 @@ const AccountSetUp = () => {
     };
 
     const handleSignUp = async () => {
+        if (!handleCheck()) {
+            let errorMsg = '';
+            if (isConfirmState !== 'SUCCESS') {
+                errorMsg = '이메일 인증을 완료해주세요.';
+            } else if (!password.length) {
+                errorMsg = '비밀번호를 입력해주세요.';
+            } else if (!passwordConfirm.length) {
+                errorMsg = '비밀번호 확인을 입력해주세요.';
+            } else {
+                errorMsg = '입력 정보를 확인해주세요.';
+            }
+
+            setToast({ message: errorMsg, isError: true });
+            setIsToastOpen(true);
+            return;
+        }
         const sex = selectedGender === '남성' ? 'MALE' : 'FEMALE';
         const res = await signUp({
             email,
@@ -83,6 +101,13 @@ const AccountSetUp = () => {
         } else {
             setIsSentState('SUCCESS');
             setToast({ message: '이메일로 인증번호가 발송되었습니다.', isError: false });
+
+            setDescriptionChange(true);
+            if (timerRef.current) clearTimeout(timerRef.current);
+            timerRef.current = setTimeout(() => {
+                setDescriptionChange(false);
+                timerRef.current = null;
+            }, 5 * 60 * 1000);
         }
         setIsToastOpen(true);
     };
@@ -102,7 +127,15 @@ const AccountSetUp = () => {
     return (
         <FlexBox direction="col" className="gap-6 md:gap-7">
             <p className="font-bold md:text-2xl text-xl text-[#394150] text-center mb-1">개인 정보 입력</p>
-            <InputContainer label="이메일 주소" description="자주 사용하는 이메일로 가입해주세요 :)">
+            <InputContainer
+                label="이메일 주소"
+                description={
+                    descriptionChange && isConfirmState !== 'SUCCESS'
+                        ? '메일을 받지 못하셨다면 스팸함도 함께 확인해보세요. :)'
+                        : '자주 사용하는 이메일로 가입해주세요 :)'
+                }
+                isStress={descriptionChange && isConfirmState !== 'SUCCESS'}
+            >
                 <FlexBox className="gap-2 md:gap-3 items-center">
                     <InputField
                         type="email"
@@ -161,7 +194,7 @@ const AccountSetUp = () => {
             </InputContainer>
             <div className="flex flex-col-reverse md:flex-row md:justify-between mt-3 gap-1">
                 <ButtonNavigate text="이전" hasBackGround={false} onClick={() => setCurrentStep(2)} />
-                <ButtonNavigate text="다음" isActive={handleCheck()} onClick={handleSignUp} />
+                <ButtonNavigate text="다음" onClick={handleSignUp} />
             </div>
             {isToastOpen && (
                 <ToastMessage
